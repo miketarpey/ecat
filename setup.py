e20ab10787b60d814912adbece8dbaaa760146c9
Mike Tarpey <miketarpey@gmx.net>
2022-02-11 11:12:56 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ecat',
+      version='0.1',
+      description='Baxter eCatalogue',
+      url='https://github.com/miketarpey/ecat',
+      author='Mike Tarpey ',
+      author_email='miketarpey@gmx.net',
+      license='MIT',
+      packages=['ecat'],
+      zip_safe=False)
