f45de9f7143716952ed2aae8402009fabc2235f4
Mike Tarpey <miketarpey@gmx.net>
2022-02-16 15:41:44 +0100

    Added sql.py comment logic

--- ecat/sql.py
+++ ecat/sql.py
@@ -3,6 +3,7 @@ import pandas as pd
 import logging
 from typing import Union, List
 from jinja2 import Environment, FileSystemLoader
+from copy import deepcopy
 
 logger = logging.getLogger(__name__)
 
@@ -30,12 +31,31 @@ def generate_sql(template_sql:str, template_values:dict,
 
     template_code = env.get_template(name=template_sql)
 
-    # I know, a hard-coded 'parameter' comment being 'manipulated
-    # to remove newline and spaces. Not great coding but can't see
-    # a better way of doing this.
-    template_values['comment'] = re.sub('\n\s+--', '\n--',
-                                        template_values['comment'])
+    # Convert 'comment' list into concatenated string delimitted with '\n--'
+    template_values_copy = deepcopy(template_values)
+    template_values_copy['comment'] = '\n-- '.join(template_values_copy['comment'])
 
-    sql = template_code.render(template_values)
+    sql = template_code.render(template_values_copy)
 
     return sql
+
+
+def series_to_str(series: pd.Series):
+    ''' Convert pandas series to a string enclosed in parentheses
+
+    Parameters
+    ----------
+    series
+        pandas Series
+
+    Returns
+    -------
+    str representation of Series object enclosed in parentheses.
+
+    '''
+
+    str_series = ', '.join(series.astype(str).tolist())
+    str_series = '(' + str_series + ')'
+
+    return str_series
+
